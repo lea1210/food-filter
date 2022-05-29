ea1c29993df5cf3dc6d6b6f8028965e422d2af8d
maxkerkmann <max.p.kerkmann@gmail.com>
2022-05-29 14:18:52 +0200

    Adjusted Footer Color, Created input form, Adjusted projekt-structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-import {Footer} from "./components/Footer/Footer";
 import {Layout} from "./components/Layout/Layout";
+
+import Page from "./pages/home";
 
 function App() {
   return (
     <div className="wrapper">
       <Layout>
-
-       <h1>Hallo</h1>
-          <div className="body">
-       <p>
-         Guten Tag
-       </p>
-          </div>
-
+        <Page/>
       </Layout>
     </div>
   );
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import {IngredientSearch} from "../../components/IngredientSearch/IngredientSearch";
+import Styles from "./index.module.css";
+
+const Page = () => {
+    return (
+        <IngredientSearch className={Styles.flex}/>
+    );
+};
+
+export default Page;
