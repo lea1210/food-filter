41cfbd6235c3753f8001842778fd3b9d1b5d631a
maxkerkmann <max.p.kerkmann@gmail.com>
2022-06-03 11:00:04 +0200

    fixed footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import Styles from  './App.module.css';
 import {Layout} from "./components/Layout/Layout";
 
 import Page from "./pages/home";
+
 
 function App() {
   return (
     <div className="wrapper">
       <Layout>
-        <Page/>
+        <Page className={Styles.page}/>
       </Layout>
     </div>
   );
