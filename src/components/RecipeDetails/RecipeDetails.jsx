2f5566b1f7d6ac95423dbe3ed6120b3190d3ba6b
Rebecca Marx <rebecca.marx@student.hs-rm.de>
2022-06-03 11:26:09 +0200

    filled RecipeView with first content and buttons

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,7 +1,23 @@
+import React from "react";
 import Styles from "../RecipeDetails/RecipeDetails.module.css";
+import {Recipe} from "../Recipe/Recipe";
+import {Button} from "../Button/Button";
+import ingredients from "../../hooks/useRecipesData";
 
-export const RecipeDetails = () => {
+export const RecipeDetails = (recipe, onClose) => {
     return (
-        <div/>
+        <>
+            <div className={Styles.Button}>
+                <Button onClick={onClose}>Abbrechen</Button>
+            </div>
+            <div className={Styles.recipeHeadline}>
+                <div className={Styles.img}>
+                    <Recipe imgUrl={recipe.imgUrl}></Recipe>
+                </div>
+                <Recipe name={recipe.name}></Recipe>
+            </div>
+        </>
     );
 };
+
+export default RecipeDetails;
