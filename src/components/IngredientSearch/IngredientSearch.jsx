e0d78af37f90d371480d6a17f2590f8cef08a807
Rebecca Marx <rebecca.marx@student.hs-rm.de>
2022-06-10 13:18:55 +0200

    tried to fix error in hook

--- src/components/IngredientSearch/IngredientSearch.jsx
+++ src/components/IngredientSearch/IngredientSearch.jsx
@@ -1,31 +1,28 @@
 import Styles from "./IngredientSearch.module.css";
-import { useRecipesData } from "../../hooks/useRecipesData";
+import { useRecipesData, loadRecipes } from "../../hooks/useRecipesData";
 
-const OnClickSearch = () => {
-    const ingredientList = ["Spaghetti", "Tomaten"];
-    const excludedList = [];
-    console.log("bin in der funktion");
-    const data = useRecipesData(["Spaghetti", "Tomate"], [], false, false, false, false);
-    console.log(data);
-
-    return (
-        <>
-        </>
-    );
-
-}
 export const IngredientSearch = () => {
-    return (
-        <div className={Styles.ingredientSearch}>
-            <h2 className={Styles.headerText}>Welche Zutaten hast du zuhause?</h2>
-            <input type="text" className={Styles.centerInput}/>
-            <div>
-                <button className={Styles.input}>Hinzufügen</button>
-                <button className={Styles.input}>Ausschließen</button>
+    console.log("lebe noch");
+
+    const OnClickSearch = () => {
+        const ingredientList = ["Spaghetti", "Tomaten"];
+        const excludedList = [];
+        console.log("bin in der funktion");
+
+        //console.log(data.loadRecipes());
+    }
+
+        return (
+            <div className={Styles.ingredientSearch}>
+                <h2 className={Styles.headerText}>Welche Zutaten hast du zuhause?</h2>
+                <input type="text" className={Styles.centerInput}/>
+                <div>
+                    <button className={Styles.input}>Hinzufügen</button>
+                    <button className={Styles.input}>Ausschließen</button>
+                </div>
+                <button className={Styles.search} onClick={OnClickSearch}>Suchen</button>
             </div>
-            <button className={Styles.search} onClick={OnClickSearch}>Suchen</button>
-        </div>
-    );
-};
+        );
+    };
 
 export default IngredientSearch;
