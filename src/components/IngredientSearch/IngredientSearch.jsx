5e69abf04b8ddcf22bb5bddceceb4752fe312053
Rebecca Marx <rebecca.marx@student.hs-rm.de>
2022-06-10 12:24:11 +0200

    created test call to get recipes

--- src/components/IngredientSearch/IngredientSearch.jsx
+++ src/components/IngredientSearch/IngredientSearch.jsx
@@ -1,5 +1,14 @@
 import Styles from "./IngredientSearch.module.css";
+import { useRecipesData } from "../../hooks/useRecipesData";
 
+const OnClickSearch = () => {
+    const ingredientList = ["Spaghetti", "Tomaten"];
+    const excludedList = [];
+
+    const data = useRecipesData(ingredientList, excludedList, false, false, false, false);
+    console.log(data);
+
+}
 export const IngredientSearch = () => {
     return (
         <div className={Styles.ingredientSearch}>
@@ -9,7 +18,7 @@ export const IngredientSearch = () => {
                 <button className={Styles.input}>Hinzufügen</button>
                 <button className={Styles.input}>Ausschließen</button>
             </div>
-            <button className={Styles.search}>Suchen</button>
+            <button className={Styles.search} onClick={OnClickSearch}>Suchen</button>
         </div>
     );
 };
