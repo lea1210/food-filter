d1bf7745e43c8b92bff78777eb44b44d42412605
Lea Rörig <lea.roerig@student.hs-rm.de>
2022-06-17 10:32:05 +0200

    fixed deleting tags

--- src/components/IngredientTags/IngredientTags.jsx
+++ src/components/IngredientTags/IngredientTags.jsx
@@ -4,23 +4,18 @@ import IngredientList from "../IngredientList/IngredientList";
 import Styles from "./IngredientTags.module.css";
 
 export const IngredientTags = () => {
-    //temporary
-    const ingredients = [{name: "käse"}, {name:"salami"}, {name:"nutella"}];
-
     const {ingredientList} = useIngredients();
     const {excludedList} = useExcludedIngredients();
-
-    //hier 2x igredeintlist, einmal ingredientlist übergebenn, einmal excluded
 
     return(
         <>
             <div className={Styles.upperBox}>
-              <label className={Styles.headline}>Zutaten</label>
+                {ingredientList.length > 0 && <label className={Styles.headline}>Das hab ich</label>}
               <IngredientList ingredients={ingredientList} color={"dark"}></IngredientList>
             </div>
             <div className={Styles.secondBox}>
-             <label className={Styles.headline}>Ausgeschlossene Zutaten</label>
-             <IngredientList ingredients={excludedList} color={"light"}></IngredientList>
+                {excludedList.length > 0  && <label className={Styles.headline}>Das mag ich nicht</label>}
+                <IngredientList ingredients={excludedList} color={"light"}></IngredientList>
             </div>
         </>
     );
