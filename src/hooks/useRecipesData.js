0afcd0a44581501f4a31016c8b117e496eb067c6
Lea Rörig <lea.roerig@student.hs-rm.de>
2022-06-29 17:44:45 +0200

    reformatted code

--- src/hooks/useRecipesData.js
+++ src/hooks/useRecipesData.js
@@ -2,10 +2,8 @@ import {useState} from "react";
 import {useIngredients} from "../contexts/IngredientContext/IngredientContext";
 import {useExcludedIngredients} from "../contexts/ExcludedContext/ExcludedContext";
 import {usePreferences} from "../contexts/PreferencesContext/PreferencesContext";
-import ingredient from "../components/Ingredient/Ingredient";
 
 const API_URL = "http://localhost:1337/api/recipes/";
-//?pagination[page]=1&pagination[pageSize]=100
 
 async function extractResult(result) {
     if (result.ok) return await result.json();
@@ -20,35 +18,28 @@ function determineFilterParams(ingredientList, excludedList, vegan, vegetarian, 
     let ingredientCounter = 0;
 
     ingredientList.map((ingredient) => {
-        if (ingredientCounter === 0){
+        if (ingredientCounter === 0) {
             ingredientParams += "?";
-        }else{
+        } else {
             ingredientParams += "&";
         }
         ingredientParams += "filters[$and][" + ingredientCounter + "][ingredients][name]=" + ingredient.name.toLowerCase();
         ingredientCounter += 1;
     });
- /*   excludedList.map((excludedIngr) => {
-        console.log("ich exclude" + excludedIngr.name);
-        excludedParams += "?filters[ingredients][name][$neq]=" + excludedIngr.name.toLowerCase() + "&";
-    });*/
-    filterParams =  ingredientParams + excludedParams.slice(0,-1);
-    if(vegan) filterParams += "&filters[vegan]=true";
-    if(vegetarian) filterParams += "&filters[vegetarian]=true";
-    if(glutenfree) filterParams += "&filters[glutenfree]=true";
-    if(lactosefree) filterParams += "&filters[lactosefree]=true";
-    return filterParams+"&pagination[page]=1&pagination[pageSize]=100&populate=*";
+
+    filterParams = ingredientParams + excludedParams.slice(0, -1);
+    if (vegan) filterParams += "&filters[vegan]=true";
+    if (vegetarian) filterParams += "&filters[vegetarian]=true";
+    if (glutenfree) filterParams += "&filters[glutenfree]=true";
+    if (lactosefree) filterParams += "&filters[lactosefree]=true";
+    return filterParams + "&pagination[page]=1&pagination[pageSize]=100&populate=*";
 }
-function removeExcludedRecipes (recipes, excludedList){
-    return recipes.filter(function (recipe){
-        console.log(recipe);
-        console.log(excludedList);
+
+function removeExcludedRecipes(recipes, excludedList) {
+    return recipes.filter(function (recipe) {
         for (let i = 0; i < recipe.attributes.ingredients.data.length; i++) {
-            console.log(recipe.attributes.ingredients.data[i].attributes.name);
             for (let j = 0; j < excludedList.length; j++) {
-                console.log("aus data: ", recipe.attributes.ingredients.data[i].attributes.name);
-                console.log("aus excludedlist: ", excludedList[j]);
-                if(excludedList[j].name === recipe.attributes.ingredients.data[i].attributes.name){
+                if (excludedList[j].name === recipe.attributes.ingredients.data[i].attributes.name) {
                     return false
                 }
             }
@@ -56,22 +47,6 @@ function removeExcludedRecipes (recipes, excludedList){
         return true;
     });
 }
-
-/*function excludeRecipe(recipe){
-    console.log(recipe);
-    console.log(excludedList);
-    for (let i = 0; i < recipe.attributes.ingredients.data.length; i++) {
-        console.log(recipe.attributes.ingredients.data[i].attributes.name);
-        for (let j = 0; j < excludedList.length; j++) {
-            console.log("aus data: ", recipe.attributes.ingredients.data[i].attributes.name);
-            console.log("aus excludedlist: ", excludedList[j]);
-            if(excludedList[j] === recipe.attributes.ingredients.data[i].attributes.name){
-                return false
-            }
-        }
-    }
-    return true;
-}*/
 
 export const fetchRecipes = async (filterParams) => {
     console.log(API_URL + filterParams);
@@ -98,9 +73,7 @@ export const useRecipesData = () => {
         const filterParams = determineFilterParams(ingredientList, excludedList, isVegan, isVegetarian, isLactosefree, isGlutenfree);
         fetchRecipes(filterParams)
             .then((recipes) => {
-                console.log("recipes vor exclude: ", recipes.data);
                 const finaleRecipes = removeExcludedRecipes(recipes.data, excludedList);
-                console.log("recipes danach: ", finaleRecipes);
                 setData(finaleRecipes);
             })
             .catch((e) => setError(e))
