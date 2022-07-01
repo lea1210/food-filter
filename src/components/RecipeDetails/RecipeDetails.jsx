3a5a6421e6027afdf9e9f1ad15194d79e65e7dfd
Rebecca Marx <rebecca.marx@student.hs-rm.de>
2022-07-01 11:28:31 +0200

    Recipe Details appears on click as extra window

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -3,19 +3,23 @@ import Styles from "../RecipeDetails/RecipeDetails.module.css";
 import {Recipe} from "../Recipe/Recipe";
 import {Button} from "../Button/Button";
 
-export const RecipeDetails = ({recipe, onClose, id }) => {
+export const RecipeDetails = ({recipe, onClose, id}) => {
     console.log(recipe);
     return (
         <>
-            <div className={Styles.Button}>
-                <Button onClick={onClose}>Abbrechen</Button>
-            </div>
-            <div className={Styles.recipe}>
-                <Recipe {...recipe.attributes}></Recipe>
-            </div>
-{/*            <div className={Styles.ButtonEasterEgg}>
+            <div className={Styles.recipeFormBackdrop}>
+                <div className={Styles.recipeForm}>
+                    <div className={Styles.Button}>
+                        <Button onClick={onClose}>X</Button>
+                    </div>
+                    <div className={Styles.recipe}>
+                        <Recipe {...recipe.attributes}></Recipe>
+                    </div>
+                    {/*            <div className={Styles.ButtonEasterEgg}>
                 <Button onClick={}>Click me</Button>
             </div>*/}
+                </div>
+            </div>
         </>
     );
 };
