7886f7f57f1d2ed1f03d249af06a33f6e44e2a5a
Rebecca Marx <rebecca.marx@student.hs-rm.de>
2022-07-01 11:18:27 +0200

    styled recipe and recipe preview

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -2,23 +2,38 @@ import Styles from "../Recipe/Recipe.module.css";
 import PropTypes from "prop-types";
 import React from "react";
 
-export const Recipe = ({name, onClick, image, ingredientlist, description, preperation, id, vegan, vegetarian, lactosefree, glutenfree}) => {
+export const Recipe = ({
+                           name,
+                           onClick,
+                           image,
+                           ingredientlist,
+                           description,
+                           preperation,
+                           id,
+                           vegan,
+                           vegetarian,
+                           lactosefree,
+                           glutenfree
+                       }) => {
     return (
-        <div data-testid="recipe" onClick={onClick}>
+        <div data-testid="recipe" className={`${Styles.recipeContainer} ${onClick ? Styles.clickable : ''}`}
+             onClick={onClick}>
             <div className={Styles.recipeDetails}>
-                <h4 className={Styles.recipeName}>{name}</h4>
-            </div>
-            <div>
-                <img src={image} className={Styles.img}/>
-            </div>
-            <div className={Styles.recipeDescription}>
-                {description}
-            </div>
-            <div className={Styles.preparation}>
-                {preperation}
-            </div>
-            <div className={Styles.ingredients}>
-                {ingredientlist}
+                <div>
+                    <img src={image} className={Styles.img}/>
+                </div>
+                <div className={Styles.textContainer}>
+                    <h4 className={Styles.recipeName}>{name}</h4>
+                    <div className={Styles.recipeDescription}>
+                        {description}
+                    </div>
+                </div>
+                <div className={Styles.preparation}>
+                    {preperation}
+                </div>
+                <div className={Styles.ingredients}>
+                    {ingredientlist}
+                </div>
             </div>
             {/*<div className={Styles.preferences}>*/}
             {/*    <li>*/}
