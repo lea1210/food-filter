6549c01cce1bdad807ccaf3a6ba82d39c42c5202
maxkerkmann <max.p.kerkmann@gmail.com>
2022-07-16 17:12:44 +0200

    added test with error

--- src/components/RegistrationForm/RegistrationForm.test.jsx
+++ src/components/RegistrationForm/RegistrationForm.test.jsx
@@ -1,17 +1,25 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import RegistrationForm from './RegistrationForm';
+import promise from 'promise';
+import { act } from 'react-dom/test-utils';
+import RegistrationFormContextWrapper from './RegistrationForm';
+
+const mockOnSubmit = jest.fn(() => promise);
 
 describe('Registration', () => {
   describe('Registration with valid input', () => {
     it('should validate an input', async () => {
-      const mockOnSubmit = jest.fn();
-      const { getByLabelText, getByRole } = render(<RegistrationForm onSubmit={mockOnSubmit} />);
-
+      const wrapper = render(<RegistrationFormContextWrapper onSubmit={mockOnSubmit} />);
+      const promise = Promise.resolve();
       fireEvent.change(screen.getByLabelText('Username*'), { target: { value: 'testuser' } });
       fireEvent.change(screen.getByLabelText('E-Mail*'), { target: { value: 'testuser' } });
       fireEvent.change(screen.getByLabelText('Passwort*'), { target: { value: 'testuser' } });
+      const button = wrapper.findByText('Registrieren');
+      fireEvent.click(button);
+      expect(mockOnSubmit).toHaveBeenCalled();
+
+      await act(() => promise);
     });
   });
 });
