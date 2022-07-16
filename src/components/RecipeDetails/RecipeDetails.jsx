54e011c13242ed525cde866da2e523d208298d48
Lea Rörig <lea.roerig@student.hs-rm.de>
2022-07-16 15:54:56 +0200

    recipe style changes

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Styles from '../RecipeDetails/RecipeDetails.module.css';
-import { Recipe } from '../Recipe/Recipe';
-import { Button } from '../Button/Button';
-import { Checkbox } from '../Checkbox/Checkbox';
 import PropTypes from 'prop-types';
 
 export const RecipeDetails = ({ image, recipe, onClose }) => {
@@ -12,29 +9,44 @@ export const RecipeDetails = ({ image, recipe, onClose }) => {
       <div className={Styles.recipeFormBackdrop}>
         <div className={Styles.recipeForm}>
           <div className={Styles.Button}>
-            <Button onClick={onClose}>X</Button>
+            <div className={Styles.delete} onClick={onClose}></div>
           </div>
           <div className={Styles.recipeContext}>
             <div className={Styles.textContainer}>
-              <div className={Styles.Container}>
-                <Recipe
-                  name={recipe.attributes.name}
-                  image={'http://localhost:1337' + image}
-                  description={recipe.attributes.description}></Recipe>
-              </div>
+              <h1 className={Styles.headline}>{recipe.attributes.name}</h1>
               <div className={Styles.preferences}>
-                <Checkbox name="vegan" checked={recipe.attributes.vegan}></Checkbox>
-                <label className={Styles.label}>vegan</label>
-                <Checkbox name="vegetarisch" checked={recipe.attributes.vegetarian}></Checkbox>
-                <label className={Styles.label}>vegetarisch</label>
-                <Checkbox name="laktosefrei" checked={recipe.attributes.lactosefree}></Checkbox>
-                <label className={Styles.label}>laktosefrei</label>
-                <Checkbox name="glutenfrei" checked={recipe.attributes.glutenfree}></Checkbox>
-                <label className={Styles.label}>glutenfrei</label>
+                {recipe.attributes.vegan ? (
+                  <label className={Styles.prefLabel}>vegan </label>
+                ) : (
+                  <label></label>
+                )}
+                {recipe.attributes.vegetarian ? (
+                  <label className={Styles.prefLabel}>vegetarisch </label>
+                ) : (
+                  <label></label>
+                )}
+                {recipe.attributes.glutenfree ? (
+                  <label className={Styles.prefLabel}>glutenfrei </label>
+                ) : (
+                  <label></label>
+                )}
+                {recipe.attributes.lactosefree ? (
+                  <label className={Styles.prefLabel}>laktosefrei</label>
+                ) : (
+                  <label></label>
+                )}
               </div>
-              <div className={Styles.ingredients}>
-                <h3 className={Styles.headlines}>Zutaten:</h3>
-                <div>{recipe.attributes.ingredientlist}</div>
+              <div className={Styles.description}>{recipe.attributes.description}</div>
+              <div className={Styles.imgingr}>
+                <img
+                  src={'http://localhost:1337' + image}
+                  alt={'Rezeptbild'}
+                  className={Styles.image}
+                />
+                <div className={Styles.ingredients}>
+                  <h3 className={Styles.headlines}>Zutaten:</h3>
+                  <pre className={Styles.ingredients}>{recipe.attributes.ingredientlist}</pre>
+                </div>
               </div>
               <div className={Styles.preparation}>
                 <h3 className={Styles.headlines}>Zubereitung:</h3>
